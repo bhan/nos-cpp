5ac8d28dad49ecb8b81f3580f3b7c509dd5c02d8
Kenny Root <kenny@the-b.org>
2014-11-24 00:38:40 -0800

    Add printing of parsed classes

--- idlgen/AstParser.cpp
+++ idlgen/AstParser.cpp
@@ -1,11 +1,31 @@
 #include <clang-c/Index.h>
 #include <iostream>
 
-enum CXChildVisitResult visitorFunc(CXCursor cursor, CXCursor parent, CXClientData data) {
-    CXType type = clang_getCursorType(cursor);
-    CXString str = clang_getTypeKindSpelling(type.kind);
-    std::cout << "Type: " << clang_getCString(str) << std::endl;
-    clang_disposeString(str);
+enum CXChildVisitResult functionPrinter(CXCursor cursor, CXCursor, CXClientData) {
+    CX_CXXAccessSpecifier access = clang_getCXXAccessSpecifier(cursor);
+    if (access != CX_CXXAccessSpecifier::CX_CXXPublic) {
+        return CXChildVisit_Continue;
+    }
+
+    if (cursor.kind == CXCursorKind::CXCursor_CXXMethod) {
+        CXString str = clang_getCursorDisplayName(cursor);
+        std::cout << "  Function: " << clang_getCString(str) << std::endl;
+        clang_disposeString(str);
+    } else if (cursor.kind == CXCursorKind::CXCursor_Constructor) {
+        CXString str = clang_getCursorDisplayName(cursor);
+        std::cout << "  Constructor: " << clang_getCString(str) << std::endl;
+        clang_disposeString(str);
+    }
+    return CXChildVisit_Continue;
+}
+enum CXChildVisitResult classPrinter(CXCursor cursor, CXCursor, CXClientData) {
+    if (cursor.kind == CXCursorKind::CXCursor_ClassDecl) {
+        CXString str = clang_getCursorDisplayName(cursor);
+        std::cout << "Class: " << clang_getCString(str) << std::endl;
+        clang_disposeString(str);
+
+        clang_visitChildren(cursor, functionPrinter, nullptr);
+    }
 
     return CXChildVisit_Continue;
 }
@@ -24,7 +44,7 @@ int main(int argc, char* argv[]) {
     CXTranslationUnit transUnit = clang_parseTranslationUnit(index, 0, argv, argc, 0, 0, CXTranslationUnit_None);
 
     CXCursor cursor = clang_getTranslationUnitCursor(transUnit);
-    clang_visitChildren(cursor, visitorFunc, nullptr);
+    clang_visitChildren(cursor, classPrinter, nullptr);
 
     clang_disposeTranslationUnit(transUnit);
     clang_disposeIndex(index);
