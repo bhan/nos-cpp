8ae77bc59c1bee03bc483e5aa4490b3a2f6e8407
Bo Han <bo.ning.han@gmail.com>
2014-12-10 20:04:32 -0800

    generate acceptor

--- paxos/AcceptorGenerated.hpp
+++ paxos/AcceptorGenerated.hpp
@@ -17,15 +17,13 @@
 enum class AcceptorMethodID : uint32_t {
     // Methods with return values
 
+    prepare,
+
+    accept,
+
     // Methods without return values
 
     // Getters and setters for public fields
-
-    getMinAcceptNum,
-    setMinAcceptNum,
-
-    getAccepted,
-    setAccepted,
 
 };
 
@@ -42,23 +40,15 @@ class AcceptorServer : public Acceptor {
     }
 
 
-
-
-    uint32_t getMinAcceptNum() {
-        return _base->_min_accept_num;
+    std::tuple<uint32_t, std::string> prepare(uint32_t num) {
+        return _base->prepare(num);
     }
 
-    void setMinAcceptNum(uint32_t &value) {
-        _base->_min_accept_num = value;
+    bool accept(std::tuple<uint32_t, std::string> accept_request) {
+        return _base->accept(accept_request);
     }
 
-    std::map<uint32_t, std::string> getAccepted() {
-        return _base->_accepted;
-    }
 
-    void setAccepted(std::map<uint32_t, std::string> &value) {
-        _base->_accepted = value;
-    }
 
   private:
     Acceptor* _base;
@@ -81,45 +71,25 @@ class AcceptorAgent : public AgentObj {
     void dispatch(RPCRequest& request, RPCResponse& response) {
         switch (static_cast<AcceptorMethodID>(request.MethodID)) {
 
-
-
-            case AcceptorMethodID::getMinAcceptNum: {
-                std::cout << "dispatch: getMinAcceptNum" << std::endl;
-                //auto args = Serializer::unpack<std::tuple<>>(request.Body);
-                //auto result = TupleFunctional::apply_nonstatic_fn(&Acceptor::getMinAcceptNum, _base, args);
-                auto result = _base->_min_accept_num;
+            case AcceptorMethodID::prepare: {
+                std::cout << "dispatch: prepare" << std::endl;
+                auto args = Serializer::unpack< std::tuple< uint32_t > >(request.Body);
+                auto result = TupleFunctional::apply_nonstatic_fn(&Acceptor::prepare, _base, args);
+                response.Code = ServerCode::OK;
                 response.Body = Serializer::pack<decltype(result)>(result);
-                response.Code = ServerCode::OK;
                 break;
             }
 
-            case AcceptorMethodID::setMinAcceptNum: {
-                std::cout << "dispatch: setMinAcceptNum" << std::endl;
-                auto args = Serializer::unpack<std::tuple<uint32_t>>(request.Body);
-                //TupleFunctional::apply_nonstatic_fn(&Acceptor::setMinAcceptNum, _base, args);
-                _base->_min_accept_num = std::get<0>(args);
+            case AcceptorMethodID::accept: {
+                std::cout << "dispatch: accept" << std::endl;
+                auto args = Serializer::unpack< std::tuple< std::tuple<uint32_t, std::string> > >(request.Body);
+                auto result = TupleFunctional::apply_nonstatic_fn(&Acceptor::accept, _base, args);
                 response.Code = ServerCode::OK;
-                break;
-            }
-
-            case AcceptorMethodID::getAccepted: {
-                std::cout << "dispatch: getAccepted" << std::endl;
-                //auto args = Serializer::unpack<std::tuple<>>(request.Body);
-                //auto result = TupleFunctional::apply_nonstatic_fn(&Acceptor::getAccepted, _base, args);
-                auto result = _base->_accepted;
                 response.Body = Serializer::pack<decltype(result)>(result);
-                response.Code = ServerCode::OK;
                 break;
             }
 
-            case AcceptorMethodID::setAccepted: {
-                std::cout << "dispatch: setAccepted" << std::endl;
-                auto args = Serializer::unpack<std::tuple<std::map<uint32_t, std::string>>>(request.Body);
-                //TupleFunctional::apply_nonstatic_fn(&Acceptor::setAccepted, _base, args);
-                _base->_accepted = std::get<0>(args);
-                response.Code = ServerCode::OK;
-                break;
-            }
+
 
             default: {
                 std::stringstream err;
@@ -144,45 +114,27 @@ class AcceptorClient : public ClientObj {
         _client->mark_obj_deleted(_name);
     }
 
-
-
-    uint32_t getMinAcceptNum() {
-        auto args = std::make_tuple();
+    std::tuple<uint32_t, std::string> prepare(uint32_t num) {
+        auto args = std::make_tuple(num);
         RPCRequest request(static_cast<uint32_t>(RequestType::invoke), _name,
-                           static_cast<uint32_t>(AcceptorMethodID::getMinAcceptNum),
+                           static_cast<uint32_t>(AcceptorMethodID::prepare),
                            Serializer::pack<decltype(args)>(args));
         RPCResponse response = _client->rpc_send(request, _address, _port);
         handle_rpc_exceptions(response);
-        return Serializer::unpack<uint32_t>(response.Body);
+        return Serializer::unpack<std::tuple<uint32_t, std::string>>(response.Body);
     }
 
-    void setMinAcceptNum(uint32_t &value) {
-        auto args = std::tuple< uint32_t > { value };
+    bool accept(std::tuple<uint32_t, std::string> accept_request) {
+        auto args = std::make_tuple(accept_request);
         RPCRequest request(static_cast<uint32_t>(RequestType::invoke), _name,
-                           static_cast<uint32_t>(AcceptorMethodID::setMinAcceptNum),
+                           static_cast<uint32_t>(AcceptorMethodID::accept),
                            Serializer::pack<decltype(args)>(args));
         RPCResponse response = _client->rpc_send(request, _address, _port);
         handle_rpc_exceptions(response);
+        return Serializer::unpack<bool>(response.Body);
     }
 
-    std::map<uint32_t, std::string> getAccepted() {
-        auto args = std::make_tuple();
-        RPCRequest request(static_cast<uint32_t>(RequestType::invoke), _name,
-                           static_cast<uint32_t>(AcceptorMethodID::getAccepted),
-                           Serializer::pack<decltype(args)>(args));
-        RPCResponse response = _client->rpc_send(request, _address, _port);
-        handle_rpc_exceptions(response);
-        return Serializer::unpack<std::map<uint32_t, std::string>>(response.Body);
-    }
 
-    void setAccepted(std::map<uint32_t, std::string> &value) {
-        auto args = std::tuple< std::map<uint32_t, std::string> > { value };
-        RPCRequest request(static_cast<uint32_t>(RequestType::invoke), _name,
-                           static_cast<uint32_t>(AcceptorMethodID::setAccepted),
-                           Serializer::pack<decltype(args)>(args));
-        RPCResponse response = _client->rpc_send(request, _address, _port);
-        handle_rpc_exceptions(response);
-    }
 
   private:
     void handle_rpc_exceptions(RPCResponse &response) {
